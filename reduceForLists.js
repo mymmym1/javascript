dfc5d6c86b8c96e7c8aa1424b95744b03582862d
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2022-01-09 17:23:50 -0500

    Create reduceForLists.js

---
+++ reduceForLists.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function string2int(s) {
+    var m = new Map([['1', 1], ['2', 2], ['3', 3], ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9], ['0', 0]]);
+    var l = [];
+    for (var i = 0; i < s.length; i++) {
+        l.push(m.get(s[i]));
+    }
+    //console.log(l);
+    var result = l.reduce(function(x, y){
+        return x * 10 + y;
+    });
+    
+    return result;
+}
