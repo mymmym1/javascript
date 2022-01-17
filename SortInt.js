9b799f744d9212eb87f1a338e3c17edf1e6f2e8a
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2022-01-17 01:43:23 -0500

    Create SortInt.js

---
+++ SortInt.js
@@ -0,0 +1,6 @@
+//javascrip sort() converts int to string, then sort. 
+//That's why it need the following:
+
+var arr = [10, 20, 1, 2];
+arr.sort((x, y) => x-y);
+console.log(arr); // [1, 2, 10, 20]
